9ca0037988826177a8e3a0dd3505732d09e50083
Rakshit Kamboj <rakshitkamboj@Rakshits-MacBook-Air.local>
2025-03-17 16:51:21 +0530

    commited upto google auth

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from './components/custom/Hero'
+import Header from './components/custom/Header'
+import './index.css'
+
+function App() {
+  return (
+    <div className=" min-h-screen flex flex-col items-center justify-center">
+     
+      
+      <Hero />
+      <Header/>
+      
+    </div>
+  )
+}
+
+export default App
