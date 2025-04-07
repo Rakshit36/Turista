a4c18d70876d68566f3ed9e3e8042fba777b6446
Rakshit Kamboj <rakshitkamboj@Rakshits-MacBook-Air.local>
2025-04-08 01:33:16 +0530

    some bugs fixed

--- src/components/Google_sign.jsx
+++ src/components/Google_sign.jsx
@@ -8,10 +8,10 @@ const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: "turista-66072.firebasestorage.app",
-    messagingSenderId: "637452293587",
-    appId: "1:637452293587:web:c932a73ef04a866ee04b26",
-    measurementId: "G-4HMZ36MHJQ"
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
 
