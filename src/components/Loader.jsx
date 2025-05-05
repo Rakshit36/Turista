0ca3d2cfc772973cdcb2490b272cd06923a420ab
Rakshit Kamboj <rakshitkamboj@Rakshits-MacBook-Air.local>
2025-05-05 14:54:37 +0530

    loader updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,34 +1,43 @@
-import React from "react";
-import "./Loader.css";
+// TransitionScreen.js
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Loader.css'
+const TransitionScreen = () => {
+  const navigate = useNavigate();
 
-const HourglassLoader = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 3000); 
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
-    <div className="loader-container">
-      <div
-        aria-label="Orange and tan hamster running in a black wheel"
-        role="img"
-        className="wheel-and-hamster"
-      >
-        <div className="wheel"></div>
-        <div className="hamster">
-          <div className="hamster__body">
-            <div className="hamster__head">
-              <div className="hamster__ear"></div>
-              <div className="hamster__eye"></div>
-              <div className="hamster__nose"></div>
-            </div>
-            <div className="hamster__limb hamster__limb--fr"></div>
-            <div className="hamster__limb hamster__limb--fl"></div>
-            <div className="hamster__limb hamster__limb--br"></div>
-            <div className="hamster__limb hamster__limb--bl"></div>
-            <div className="hamster__tail"></div>
-          </div>
-        </div>
-        <div className="spoke"></div>
+    <div className="flex items-center justify-center min-h-screen bg-white text-blue-600">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4 animate-pulse">Please Wait </h1>
+        {/* <p className="text-lg text-black font-bold">Your trip is being genrated</p> */}
+        <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+	<div className="wheel"></div>
+	<div className="hamster">
+		<div className="hamster__body">
+			<div className="hamster__head">
+				<div className="hamster__ear"></div>
+				<div className="hamster__eye"></div>
+				<div className="hamster__nose"></div>
+			</div>
+			<div className="hamster__limb hamster__limb--fr"></div>
+			<div className="hamster__limb hamster__limb--fl"></div>
+			<div className="hamster__limb hamster__limb--br"></div>
+			<div className="hamster__limb hamster__limb--bl"></div>
+			<div className="hamster__tail"></div>
+		</div>
+	</div>
+	<div className="spoke"></div>
+</div>
       </div>
- 
     </div>
   );
 };
 
-export default HourglassLoader;
+export default TransitionScreen;
